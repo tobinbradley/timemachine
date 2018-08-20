8e90e27b97328b80b8f76c9da72bf24902cad66b
tobinbradley <tobin.bradley@gmail.com>
2018-08-20 16:36:42 -0400

    high rest imagery

--- app/js/layers.js
+++ app/js/layers.js
@@ -1,86 +1,91 @@
 const aerials = {
-  "2018": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2018/{z}/{x}/{y}.jpg",
+  '2018': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2018-high/{z}/{x}/{y}.jpg',
+    minZoom: 11,
+    maxZoom: 18
+  },
+  '2017': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2017/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2017": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2017/{z}/{x}/{y}.jpg",
+  '2016': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2016/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2016": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2016/{z}/{x}/{y}.jpg",
+  '2015': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2015/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2015": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2015/{z}/{x}/{y}.jpg",
+  '2014': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2014/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2014": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2014/{z}/{x}/{y}.jpg",
+  '2013': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2013/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2013": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2013/{z}/{x}/{y}.jpg",
+  '2012': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2012/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2012": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2012/{z}/{x}/{y}.jpg",
+  '2011': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2011/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2011": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2011/{z}/{x}/{y}.jpg",
+  '2010': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2010/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2010": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2010/{z}/{x}/{y}.jpg",
+  '2009': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2009/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2009": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2009/{z}/{x}/{y}.jpg",
+  '2007': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2007/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2006": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2006/{z}/{x}/{y}.jpg",
+  '2006': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2006/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2005": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2005/{z}/{x}/{y}.jpg",
+  '2005': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2005/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2004": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2004/{z}/{x}/{y}.jpg",
+  '2004': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2004/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "2002": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/2002/{z}/{x}/{y}.jpg",
+  '2002': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/2002/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "1997": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/1997/{z}/{x}/{y}.jpg",
+  '1997': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/1997/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "1993": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/1993/{z}/{x}/{y}.jpg",
+  '1993': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/1993/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   },
-  "1978": {
-    url: "http://maps.co.mecklenburg.nc.us/tiles/1978/{z}/{x}/{y}.jpg",
+  '1978': {
+    url: 'http://maps.co.mecklenburg.nc.us/tiles/1978/{z}/{x}/{y}.jpg',
     minZoom: 9,
     maxZoom: 17
   }
@@ -88,4 +93,4 @@ const aerials = {
 
 const years = Object.keys(aerials);
 
-export { aerials, years };
+export {aerials, years};
