d571ed3b3bfe3461c8aaf692300e0dc2a6359da3
tobinbradley <tobin.bradley@gmail.com>
2020-04-06 18:15:32 -0400

    simplify code

--- src/components/yearcontrolafter.vue
+++ src/components/yearcontrolafter.vue
@@ -1,21 +1,18 @@
 <template>
 <div>
-  <a href="#" v-on:click="changeYear(-1)" v-bind:class="{ inactive: atStart }">&lt;</a>
+  <a v-if="sharedState.afterYear !== sharedState.years[sharedState.years.length - 1]"  href="#" v-on:click="changeYear(1)" >&lt;</a>
   <select v-model="sharedState.afterYear">
     <option v-for="year in sharedState.years" :selected="sharedState.afterYear === year">
        {{ year }}
     </option>
   </select>
-  <a href="#" v-on:click="changeYear(1)" v-bind:class="{ inactive: atEnd }">&gt;</a>
+  <a v-if="sharedState.afterYear !== sharedState.years[0]" href="#" v-on:click="changeYear(-1)" >&gt;</a>
 </div>
 </template>
 
 <script>
 export default {
   name: "yearcontrol",
-  watch: {
-    "sharedState.afterYear": "fixArrows"
-  },
   methods: {
     changeYear: function(inc) {
       let _this = this;
@@ -23,16 +20,6 @@ export default {
       if (loc + inc >= 0 && loc + inc <= _this.sharedState.years.length - 1) {
         _this.sharedState.afterYear = _this.sharedState.years[loc + inc];
       }
-    },
-    fixArrows: function() {
-      let _this = this;
-      _this.sharedState.years.indexOf(_this.sharedState.afterYear) === 0
-        ? (_this.atStart = true)
-        : (this.atStart = false);
-      _this.sharedState.years.indexOf(_this.sharedState.afterYear) ===
-      _this.sharedState.years.length - 1
-        ? (_this.atEnd = true)
-        : (this.atEnd = false);
     }
   }
 };
--- src/components/yearcontrolbefore.vue
+++ src/components/yearcontrolbefore.vue
@@ -1,21 +1,18 @@
 <template>
 <div>
-  <a href="#" v-on:click="changeYear(-1)" v-bind:class="{ inactive: atStart }">&lt;</a>
+  <a v-if="sharedState.beforeYear !== sharedState.years[sharedState.years.length - 1]"  href="#" v-on:click="changeYear(1)" >&lt;</a>
   <select v-model="sharedState.beforeYear">
   <option v-for="year in sharedState.years" :selected="sharedState.beforeYear === year">
      {{ year }}
   </option>
 </select>
-  <a href="#" v-on:click="changeYear(1)" v-bind:class="{ inactive: atEnd }">&gt;</a>
+  <a v-if="sharedState.beforeYear !== sharedState.years[0]" href="#" v-on:click="changeYear(-1)" >&gt;</a>
 </div>
 </template>
 
 <script>
 export default {
   name: "yearcontrol",
-  watch: {
-    "sharedState.beforeYear": "fixArrows"
-  },
   methods: {
     changeYear: function(inc) {
       let _this = this;
@@ -23,16 +20,6 @@ export default {
       if (loc + inc >= 0 && loc + inc <= _this.sharedState.years.length - 1) {
         _this.sharedState.beforeYear = _this.sharedState.years[loc + inc];
       }
-    },
-    fixArrows: function() {
-      let _this = this;
-      _this.sharedState.years.indexOf(_this.sharedState.beforeYear) === 0
-        ? (_this.atStart = true)
-        : (this.atStart = false);
-      _this.sharedState.years.indexOf(_this.sharedState.beforeYear) ===
-      _this.sharedState.years.length - 1
-        ? (_this.atEnd = true)
-        : (this.atEnd = false);
     }
   }
 };
