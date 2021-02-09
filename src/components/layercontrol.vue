9ed3342a9df4b75c41f2e17bf7b9e793f1782d24
tobinbradley <tobin.bradley@gmail.com>
2021-02-09 13:53:51 -0500

    future single component for both layer controls

---
+++ src/components/layercontrol.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <a v-if="year !== years[sharedState.years.length - 1]"  href="#" v-on:click="changeYear(1)" >&lt;</a>
+  <select v-model="year">
+    <option v-for="el in years" :selected="el === year">
+       {{ year }}
+    </option>
+  </select>
+  <a v-if="year !== years[0]" href="#" v-on:click="changeYear(-1)" >&gt;</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "yearcontrol",
+  methods: {
+    changeYear: function(inc) {
+      const idx = this.years.indexOf(this.year)
+      this.year = this.years[idx + inc]
+    }
+  }
+};
+</script>
