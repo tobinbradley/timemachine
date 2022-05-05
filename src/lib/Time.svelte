8546a785f843a98b76cfdf397e495bfbab3190a0
Tobin Bradley <tobin.bradley@gmail.com>
2022-05-05 12:46:44 -0400

    formatting

--- src/lib/Time.svelte
+++ src/lib/Time.svelte
@@ -8,7 +8,7 @@
 
 {#if $timeStart}
 <div class="px-2 flex items-center gap-2 bg-white py-1 w-full" class:accent={isPortal}>
-  <div class="w-7 text-center">
+  <div class="w-7 text-center print:hidden">
     {#if !isPortal}
     <svg class="w-5 h-7 inline fill-gray-700"><use xlink:href="#icon-map"></use></svg>
     {:else}
@@ -16,15 +16,16 @@
     {/if}
   </div>
   <div class="flex-grow">
-    <input class="w-full" type="range" step="86400" bind:value={$dte} min="{$timeStart}" max="{new Date().getTime() / 1000}" list="steplist">
+    <input class="w-full print:hidden" type="range" bind:value={$dte} min="{$timeStart}" max="{new Date().getTime()}" list="steplist">
     <datalist id="steplist">
+      <option>{new Date().getTime()}</option>
       {#each $aerials as survey}
         <option>{survey.flydate}</option>
       {/each}
     </datalist>
   </div>
   <div>
-    <h2 class="text-sm font-medium whitespace-nowrap px-1">{new Date($dte * 1000).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</h2>
+    <h2 class="text-sm font-medium whitespace-nowrap w-24 text-center">{new Date($dte).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</h2>
   </div>
 </div>
 {/if}
