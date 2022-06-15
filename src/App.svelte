8be9778b5f6f3b792ca3ba8879567af1bea69bed
tobinbradley <tobin.bradley@gmail.com>
2022-06-15 18:37:05 -0400

    help video

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,18 @@
 <script>
   import Map from './lib/Map.svelte'
   import Time from './lib/Time.svelte'
+  import Help from './lib/Help.svelte'
   import { untilDate, portalDate, portalOpen } from './store'
 </script>
 
 <main class="bg-gray-300 w-screen h-screen flex flex-col">
-  <div class="p-2 bg-white flex items-center shadow z-10">
-    <div class="text-2xl font-semibold flex-grow">
+  <div class=" bg-white flex items-center shadow z-10">
+    <div class="text-2xl font-semibold flex-grow p-2">
       <a href="./"><span class="font-bold text-emerald-700 tracking-widest">TIME</span>
       MACHINE</a>
     </div>
     <div>
-      <a href="https://gis.mecknc.gov" class="text-emerald-700">Mecklenburg County GIS</a>
+      <Help />
     </div>
   </div>
   <div class="flex-grow relative">
