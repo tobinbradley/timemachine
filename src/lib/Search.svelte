63559e260029e9d6feb0ad403e4e3f384c621e3c
tobinbradley <tobin.bradley@gmail.com>
2023-11-30 17:32:04 -0500

    switch api server

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,17 +1,17 @@
 <script>
   import AutoComplete from './Autocomplete.svelte'
   import { location } from '../store.js'
-  
+
   function jsonToURL(obj) {
     return Object.keys(obj)
       .map(i => i + '=' + encodeURIComponent(obj[i]))
       .join('&')
   }
-  
+
   let items = []
   let nomatch = false
   let spinner = false
-  
+
   // set store to selected value
   function handleHit(event) {
     location.set({
@@ -22,36 +22,36 @@
       groundpid: event.detail.groundpid
     })
   }
-  
+
   // fetch suggestions
   async function handleQuery(event) {
     const queryString = event.detail.trim()
     const urls = []
-  
+
     // address
     const addressArg = {
       columns: "full_address as value, 'ADDRESS' as type, groundpid, round(ST_X(ST_Transform(the_geom, 4326))::NUMERIC,4) as lng, round(ST_Y(ST_Transform(the_geom, 4326))::NUMERIC,4) as lat, num_parent_parcel as pid, full_address as address",
       limit: 8,
       filter: `ts @@ to_tsquery('addressing_en', '${queryString.toUpperCase().replace(/ /g, '&') + ':*'}') and cde_status='A' and the_geom is not null`
     }
-    urls.push(`https://api.mcmap.org/v1/query/master_address_table?${jsonToURL(addressArg)}`)
-  
+    urls.push(`https://maps.mecknc.gov/api/v1/query/master_address_table?${jsonToURL(addressArg)}`)
+
     // parks
     const parkArg = {
       columns: `prkname as value, 'PARK' as type, round(ST_X(ST_Transform(p.the_geom, 4326))::NUMERIC,4) as lng, round(ST_Y(ST_Transform(p.the_geom, 4326))::NUMERIC,4) as lat, t.pid as pid, prkaddr as address`,
       limit: 5,
       filter: `prkname ilike '%${queryString}%' and p.the_geom && t.the_geom`
     }
-    urls.push(`https://api.mcmap.org/v1/query/parks p, tax_parcels t?${jsonToURL(parkArg)}`)
-  
+    urls.push(`https://maps.mecknc.gov/api/v1/query/parks p, tax_parcels t?${jsonToURL(parkArg)}`)
+
     // libraries
     const libraryArg = {
       columns: `name as value, 'LIBRARY' as type, round(ST_X(ST_Transform(l.the_geom, 4326))::NUMERIC,4) as lng, round(ST_Y(ST_Transform(l.the_geom, 4326))::NUMERIC,4) as lat, p.pid as pid, address`,
       limit: 5,
       filter: `name ilike '%${queryString}%' and l.the_geom && p.the_geom`
     }
-    urls.push(`https://api.mcmap.org/v1/query/libraries l, tax_parcels p?${jsonToURL(libraryArg)}`)
-  
+    urls.push(`https://maps.mecknc.gov/api/v1/query/libraries l, tax_parcels p?${jsonToURL(libraryArg)}`)
+
     // pid
     if (!isNaN(queryString) && queryString.length >= 7) {
       const pidArg = {
@@ -59,9 +59,9 @@
         limit: 5,
         filter: `num_parent_parcel like '${queryString}%' and the_geom is not null and cde_status='A'`
       }
-      urls.push(`https://api.mcmap.org/v1/query/master_address_table?${jsonToURL(pidArg)}`)
+      urls.push(`https://maps.mecknc.gov/api/v1/query/master_address_table?${jsonToURL(pidArg)}`)
     }
-  
+
     // Fetch all the things
     spinner = true
     Promise.all(urls.map(url =>
@@ -76,9 +76,9 @@
       })
       if (items.length === 0) nomatch = true
     })
-  
+
   }
   </script>
-  
-  
+
+
   <AutoComplete placeholder="Try '2145 Suttle' or 'Jetton'" minChar="4" nomatch={nomatch} {items} on:hit={handleHit} on:query={handleQuery} value={$location.address} spinner={spinner} />
